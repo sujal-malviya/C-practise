73bc4c3f5dc1047ac78582018d9fca6a6d1e0115
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-20 06:26:56 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int sum=0,num,i;
+    printf("enter the value of num :");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        sum=i+sum;
+        if(sum==55)
+        {
+            printf("%d",sum);
+        }
+    }
+
+    return 0;
+}
