9fc89746efbbe4d0a496e89ff678b4f8111cd5bf
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-22 01:18:03 -0700

    Add files via upload

---
+++ main (33).c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=5;i++)//outer loop,row operation; 
+    {
+        for(j=5;j>i;j--)//inner loop,left-side spaces covering;
+    {
+        printf(" ");
+    }
+    for(k=1;k<=i;k++)//inner loop,*,middle space + right side spaces;
+    {
+        printf("%d ",k);
+    }
+    printf("\n");//new line;
+    }
+    return 0;
+}
