ad9592037ea57182f7067aa464465adcfb2739ba
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-23 06:28:11 -0700

    Add files via upload

---
+++ main (38).c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    scanf("%d ",&age);
+    if(age>=18)
+    {
+        printf("you can vote ");
+        
+    }
+    else
+    {
+        printf("you can't vote");
+    }
+
+    return 0;
+}
