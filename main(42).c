1a0add82e44ffe45acb5a0d48b47ac40879cfb6c
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-24 00:16:47 -0700

    Add files via upload

---
+++ main(42).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int a[5]={60,70,80,90,100};//data stored in array;
+    for(i=0;i<5;i++)
+    {
+        printf("%d ",a[i]);//printing array data;
+    
+    }
+    return 0;
+}
