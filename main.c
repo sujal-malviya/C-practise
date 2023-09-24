9ff25b7e54a1533e99628fc8b13cb79f3f3b5fb9
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-24 05:02:28 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int area ,perimeter,breadth=7,width=5;
+    area=breadth*width;
+    perimeter=2*(breadth+width);
+    printf("perimeter of rectangle is %d",perimeter);
+    printf("\narea of rectange is %d",area);
+    
+
+    return 0;
+}
