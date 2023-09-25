83c7a87c9ce4ae533682bfae3efdff6efa59d944
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-25 08:18:15 -0700

    Add files via upload

--- main (8).c
+++ main (8).c
@@ -1,15 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 
-int main()
+int main()//input number get reverse number;
 {
-    int i,n,j;
-    printf("enter the value of n :");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int a[5],i,j,n,count=0;
+    printf("enter the value :");
+    scanf("%d ",&n);
+    for(i=0;i<n;i++)
     {
-        for(j=1;j<=i;j++)
-        {
-            printf("*");
-        }printf("\n");
+        printf("\nelement is ");
+        scanf("%d ",&a[i]);//input array elements;
     }
+    /* for copying one array into other ;*/
+        for(i=0;i<n;i++)
+    for(j=i+1;j<n;j++)
+    {
+        a[i]==a[j];
+        count++;
+        break;
+        
+    }
+    printf("duplicate elmennt is %d",count);//duplicate elemnts;
+
+    return 0;
 }
