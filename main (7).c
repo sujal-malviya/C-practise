8e2dd2eab81cd56db209053fecbb9a4976c3d609
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-25 08:04:33 -0700

    Add files via upload

--- main (7).c
+++ main (7).c
@@ -1,14 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 
-int main()
+int main()//input number get reverse number;
 {
-    int i,n,sum =0;
-    printf("enter the value of n :");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int a1[5],a2[5],i,n;
+    printf("enter the value :");
+    scanf("%d ",&n);
+    for(i=0;i<n;i++)
     {
-        printf("%d ",2*i-1);
-        sum+=2*i-1;
+        printf("\nelement is ");
+        scanf("%d ",&a1[i]);//input array elements;
+    }
+    /* for copying one array into other ;*/
+    for(i=0;i<n;i++)
+    {
+        a2[i]=a1[i];//assigning value to other array;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a2[i]);//printing second array;
         
-    }printf("\nsum of %d natural odd number is %d\n",n,sum);
+    }
+
+    return 0;
 }
