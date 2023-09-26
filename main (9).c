8c9f5e823ee71f4c79e541169deaad8437d73c23
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-25 21:06:19 -0700

    Add files via upload

--- main (9).c
+++ main (9).c
@@ -1,15 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    int i,n,j;
-    printf("enter the value of n :");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-            printf("*");
-        }printf("\n");
+    int arr[] = {1, 2, 2, 3, 3};
+int n = sizeof(arr) / sizeof(arr[0]);//n=5;
+int x = 2;
+int count = 0;
+
+for (int i = 0; i < n; i++) {
+    if (arr[i] == x) {//condition will be check;
+        count++;//will count no. of duplicate element;
     }
+}
+
+printf("The element %d appears %d times in the array.", x, count);
+
+
+    return 0;
 }
