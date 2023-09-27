90d23458c47a19dcc82eb39666beedff8c10241f
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-26 23:10:22 -0700

    Add files via upload

--- main (12).c
+++ main (12).c
@@ -1,15 +1,25 @@
-#include <stdio.h>
+/******************************************************************************
 
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+void printhello();//function declaration or function prototype;
+void goodbye();
 int main()
 {
-    int i,n,j;
-    printf("enter the value of n :");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=i;j++)
-        {
-            printf("%d",i);
-        }printf("\n");
-    }
+    printhello();//function call;
+    goodbye();
+    return 0;
 }
+void printhello()//function defination;
+{
+    printf("hello!\n");
+}
+void goodbye(){
+    printf("goodbye!\n");
+}
+//function to print hello and goodbye;
