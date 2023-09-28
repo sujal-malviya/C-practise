eec47530917263589fac3fb284005a54a443e4ae
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-28 01:59:35 -0700

    Add files via upload

--- main (25).c
+++ main (25).c
@@ -1,32 +1,14 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-    int i ,j;
-    for(i=1;i<=5;i++)//it is used for row operation;
-    {
-        for(j=1;j<=5;j++)//it is used for space operation;
-        {
-            if(i==1||i==5||j==1||j==5)//we can use if-else for operation to print asterisk;
-            {
-                printf("*");
-            }
-             else
-                {
-                    printf(" ");//for space also.
-                }
-        }
-    
-        printf("\n");//move to new line;
-    }
-
-    return 0;//this is program of square of asterisk with space inside it;
-}
+    int x = 9;
+    int *ptr;
+    ptr = &x;
+    //%u is unsigned int datatype;
+    printf("address of ptr = %u",ptr);//old memory;
+    ptr++;//datatype size will be added into address;
+    printf("\naddress of ptr = %u",ptr);//neew memory;
+    ptr--;//pointer decrement;
+    printf("\naddress of ptr = %u",ptr);//size decreased by 4 byte ;
+    return 0;
+}//in pointer only we can do increment and decrement;
