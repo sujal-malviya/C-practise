aab1b160512f15066541b2fdbe7c9a4f4a09972b
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-28 22:59:26 -0700

    Add files via upload

--- main (31).c
+++ main (31).c
@@ -7,21 +7,25 @@ Welcome to GDB Online.
 
 *******************************************************************************/
 #include <stdio.h>
-
+void storetable(int a[][10], int n , int m ,int number);
 int main()
 {
-    int i,j,k;
-    for(i=1;i<=5;i++)//outer loop,row operation; 
-    {
-        for(j=5;j>i;j--)//inner loop,left-side spaces covering;
-    {
-        printf(" ");
+    int tables[2][10];
+    storetable(tables,0,10,2);
+    storetable(tables,1,10,3);
+    
+    for(int i=0;i<10;i++){
+        printf("%d\t",tables[0][i]);
     }
-    for(k=1;k<=i;k++)//inner loop,*,middle space + right side spaces;
-    {
-        printf("* ");
-    }
-    printf("\n");//new line;
+    for(int i=0;i<10;i++){
+        printf("%d\t",tables[1][i]);
     }
     return 0;
 }
+void storetable(int a[][10], int n , int m ,int number)
+{
+    for(int i=0;i<m;i++){
+        a[n][i]=number*(i+1);
+    }
+}
+//printing table using multidimensional array;
