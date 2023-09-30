22fab7b949c6438e0384fa772d4dab4b2c8e9eb6
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-30 02:56:01 -0700

    Add files via upload

--- main (39).c
+++ main (39).c
@@ -7,24 +7,22 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
-
+#include<string.h>
+void strslice(char str[],int n, int m);
 int main()
 {
-    int marks;
-    scanf("%d ",&marks);
-    if(marks>=80)
-    {
-        printf("student obtain A grade");
-        
-    }
-    else if(marks>=60)
-    {
-        printf("student obtain B grade ");
-    }
-    else
-    {
-        printf("student obtain below B grade ");
-    }
-
+    char str[100]="radheradhe!";
+    strslice(str,3,7);
+    
     return 0;
 }
+void strslice(char str[],int n,int m){//from n&m it will take that letters and print that;
+    char newstr[100];
+    int j =0;
+    for(int i=n;i<=m;i++,j++){
+        newstr[j]=str[i];
+    }
+    newstr[j]='\0';
+    puts(newstr);
+}
+//string slicing;
