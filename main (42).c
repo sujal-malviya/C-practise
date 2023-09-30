4fefd73ee1ecd53192d87980a3bb1825860c9e60
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-30 03:09:43 -0700

    Add files via upload

---
+++ main (42).c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<string.h>
+int countvowel(char str[]){
+    int count =0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+            count++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    char str[100]="radheradhe!";
+    printf("count vowel : %d",countvowel(str));
+    
+    return 0;
+}
+//count vowels in a string;
