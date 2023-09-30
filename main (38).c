c35d5e524afeb9fa30d90b1d710f2a4da09a38e0
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-09-30 02:36:48 -0700

    Add files via upload

--- main (38).c
+++ main (38).c
@@ -7,20 +7,21 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
+#include<string.h>
+void salting(char oldpass[]);
 
 int main()
 {
-    int age;
-    scanf("%d ",&age);
-    if(age>=18)
-    {
-        printf("you can vote ");
-        
-    }
-    else
-    {
-        printf("you can't vote");
-    }
-
+    char oldpass[100];
+    scanf("%s",oldpass);
+    salting(oldpass);
+    
     return 0;
+}
+void salting(char oldpass[]){
+    char salt[] ="122";
+    char newpass[200];
+    strcpy(newpass,oldpass);//it will copy old.. into new..;
+    strcat(newpass,salt);//it will concate ne.. and salt;
+    puts(newpass);//output as newpass;
 }
