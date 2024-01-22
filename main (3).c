733897adb9bd2ed4e2668c37eefa07da541f84b0
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2024-01-22 11:14:08 -0800

    Add files via upload

--- main (3).c
+++ main (3).c
@@ -1,24 +1,81 @@
-/******************************************************************************
+// C++ program to find minimum value node in binary search
+// Tree.
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+#include <vector.h>
+using namespace std;
+
+struct node {
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+
+struct node* newNode(int data)
+{
+	struct node* node
+		= (struct node*)malloc(sizeof(struct node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
+}
+
+
+struct node* insert(struct node* node, int data)
+{
+	
+	if (node == NULL)
+		return (newNode(data));
+	else {
+		/* 2. Otherwise, recur down the tree */
+		if (data <= node->data)
+			node->left = insert(node->left, data);
+		else
+			node->right = insert(node->right, data);
+
+		/* return the (unchanged) node pointer */
+		return node;
+	}
+}
+
+
+Inorder is LEFT,ROOT,RIGHT*/
+void inorder(struct node* node, vector<int>& sortedInorder)
+{
+	if (node == NULL)
+		return;
+
+	inorder(node->left, sortedInorder);
+
+	
+	sortedInorder.push_back(node->data);
+
+	
+	inorder(node->right, sortedInorder);
+}
+
 
 int main()
 {
-    int a,b,sum,average;
-    scanf("%d\n",&a);
-    scanf("%d\n",&b);
-    printf("value of a is %d\n",a);
-    printf("\nvalue of b is%d\n ",b);
-    
-    sum=a+b;
-    average=sum/2;
-    printf("sum of aand b is %d\n",sum);
-    printf("average of a and b is %d\n",average);
-    return 0;
+	struct node* root = NULL;
+	root = insert(root, 4);
+	insert(root, 2);
+	insert(root, 1);
+	insert(root, 3);
+	insert(root, 6);
+	insert(root, 4);
+	insert(root, 5);
+
+	vector<int> sortedInorder;
+	inorder(
+		root,
+		sortedInorder); 
+	printf("\n Minimum value in BST is %d",
+		sortedInorder[0]);
+	getchar();
+	return 0;
 }
