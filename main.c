9918297cce1a940ff6cf7dd8505d1825c565b88e
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2024-01-24 00:15:25 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,93 +1,100 @@
+// Including necessary header files
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a node in a linked list
-struct node {
-    int num;                // Data of the node
-    struct node *nextptr;   // Address of the next node
-} *stnode;                  // Pointer to the starting node
+// Structure for a binary tree node
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
 
-// Function prototypes
-void createNodeList(int n); // Function to create the linked list
-void displayList();         // Function to display the linked list
+// Function to create a new node
+struct TreeNode* createNode(int value) {
+    // Allocate memory for a new TreeNode
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
 
-// Main function
+    // Check if memory allocation was successful
+    if (newNode != NULL) {
+        // Initialize node data
+        newNode->data = value;
+        newNode->left = NULL;
+        newNode->right = NULL;
+    }
+
+    // Return the created node
+    return newNode;
+}
+
+// Function to insert a node into the binary tree
+struct TreeNode* insertNode(struct TreeNode* root, int value) {
+    // If the tree is empty, create a new node
+    if (root == NULL) {
+        return createNode(value);
+    }
+
+    // If the value is less than the root's data, insert into the left subtree
+    if (value < root->data) {
+        root->left = insertNode(root->left, value);
+    }
+    // If the value is greater than the root's data, insert into the right subtree
+    else if (value > root->data) {
+        root->right = insertNode(root->right, value);
+    }
+
+    // Return the modified root
+    return root;
+}
+
+// Function to perform in-order traversal of the binary tree
+void inOrderTraversal(struct TreeNode* root) {
+    // Traverse the tree in-order: left subtree, root, right subtree
+    if (root != NULL) {
+        inOrderTraversal(root->left);
+        printf("%d ", root->data);
+        inOrderTraversal(root->right);
+    }
+}
+
+// Function to free the memory allocated for the binary tree
+void freeTree(struct TreeNode* root) {
+    // Recursively free memory for the entire tree
+    if (root != NULL) {
+        freeTree(root->left);
+        freeTree(root->right);
+        free(root);
+    }
+}
+
 int main() {
-    int n;
+    // Initialize the root of the binary tree
+    struct TreeNode* root = NULL;
+    int nodeValue;
+    char choice;
 
-    // Displaying the purpose of the program
-    printf("\n\n Linked List : To create and display Singly Linked List :\n");
-    printf("-------------------------------------------------------------\n");
+    // User input loop to insert nodes into the binary tree
+    do {
+        // Prompt user for a value to insert into the binary tree
+        printf("Input a value to insert into the binary tree: ");
+        scanf("%d", &nodeValue);
 
-    // Inputting the number of nodes for the linked list
-    printf(" Input the number of nodes : ");
-    scanf("%d", &n);
+        // Insert the value into the binary tree
+        root = insertNode(root, nodeValue);
 
-    // Creating the linked list with n nodes
-    createNodeList(n);
+        // Ask user if they want to insert another node
+        printf("Want to insert another node? (y/n): ");
+        scanf(" %c", &choice);
 
-    // Displaying the data entered in the linked list
-    printf("\n Data entered in the list : \n");
-    displayList();
+    } while (choice == 'y' || choice == 'Y');
 
+    // Display the in-order traversal of the binary tree
+    printf("\nIn-order Traversal of the Binary Tree: ");
+    inOrderTraversal(root);
+    printf("\n");
+
+    // Free allocated memory for the binary tree
+    freeTree(root);
+
+    // Return 0 to indicate successful execution
     return 0;
-}
-
-// Function to create a linked list with n nodes
-void createNodeList(int n) {
-    struct node *fnNode, *tmp;
-    int num, i;
-
-    // Allocating memory for the starting node
-    stnode = (struct node *)malloc(sizeof(struct node));
-
-    // Checking if memory allocation is successful
-    if(stnode == NULL) {
-        printf(" Memory can not be allocated.");
-    } else {
-        // Reading data for the starting node from user input
-        printf(" Input data for node 1 : ");
-        scanf("%d", &num);
-        stnode->num = num;      
-        stnode->nextptr = NULL; // Setting the next pointer to NULL
-        tmp = stnode;
-
-        // Creating n nodes and adding them to the linked list
-        for(i = 2; i <= n; i++) {
-            fnNode = (struct node *)malloc(sizeof(struct node));
-
-            // Checking if memory allocation is successful
-            if(fnNode == NULL) {
-                printf(" Memory can not be allocated.");
-                break;
-            } else {
-                // Reading data for each node from user input
-                printf(" Input data for node %d : ", i);
-                scanf(" %d", &num);
-
-                fnNode->num = num;      // Setting the data for fnNode
-                fnNode->nextptr = NULL; // Setting the next pointer to NULL
-
-                tmp->nextptr = fnNode;  // Linking the current node to fnNode
-                tmp = tmp->nextptr;     // Moving tmp to the next node
-            }
-        }
-    }
-}
-
-// Function to display the linked list
-void displayList() {
-    struct node *tmp;
-
-    if(stnode == NULL) {
-        printf(" List is empty.");
-    } else {
-        tmp = stnode;
-
-        // Traversing the linked list and printing each node's data
-        while(tmp != NULL) {
-            printf(" Data = %d\n", tmp->num); // Printing the data of the current node
-            tmp = tmp->nextptr;               // Moving to the next node in the list
-        }
-    }
 }
