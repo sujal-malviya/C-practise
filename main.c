a5fb5d81287ea8f45fbf5d1ff9cf98ac9d7eceb7
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2024-01-25 23:18:25 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,100 +1,114 @@
-// Including necessary header files
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a binary tree node
-struct TreeNode {
-    int data;
-    struct TreeNode* left;
-    struct TreeNode* right;
-};
+struct node 
+{
+    int num;                    // Data of the node
+    struct node *nextptr;       // Address of the node
+}*stnode;                       // Pointer to the starting node
 
-// Function to create a new node
-struct TreeNode* createNode(int value) {
-    // Allocate memory for a new TreeNode
-    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+// Function prototypes
+void createNodeList(int n);     // Function to create the linked list
+void FirstNodeDeletion();	    // Function to delete the first node
+void displayList();             // Function to display the linked list
 
-    // Check if memory allocation was successful
-    if (newNode != NULL) {
-        // Initialize node data
-        newNode->data = value;
-        newNode->left = NULL;
-        newNode->right = NULL;
-    }
+// Main function
+int main()
+{
+    int n, num, pos;
+	
+    printf("\n\n Linked List : Delete first node of Singly Linked List :\n");
+    printf("------------------------------------------------------------\n");
 
-    // Return the created node
-    return newNode;
-}
+    printf(" Input the number of nodes : ");
+    scanf("%d", &n);
+    createNodeList(n);
 
-// Function to insert a node into the binary tree
-struct TreeNode* insertNode(struct TreeNode* root, int value) {
-    // If the tree is empty, create a new node
-    if (root == NULL) {
-        return createNode(value);
-    }
+    printf("\n Data entered in the list are : \n");		
+    displayList();
 
-    // If the value is less than the root's data, insert into the left subtree
-    if (value < root->data) {
-        root->left = insertNode(root->left, value);
-    }
-    // If the value is greater than the root's data, insert into the right subtree
-    else if (value > root->data) {
-        root->right = insertNode(root->right, value);
-    }
+    FirstNodeDeletion();
+    printf("\n Data, after deletion of first node : \n");		
+    displayList();
 
-    // Return the modified root
-    return root;
-}
-
-// Function to perform in-order traversal of the binary tree
-void inOrderTraversal(struct TreeNode* root) {
-    // Traverse the tree in-order: left subtree, root, right subtree
-    if (root != NULL) {
-        inOrderTraversal(root->left);
-        printf("%d ", root->data);
-        inOrderTraversal(root->right);
-    }
-}
-
-// Function to free the memory allocated for the binary tree
-void freeTree(struct TreeNode* root) {
-    // Recursively free memory for the entire tree
-    if (root != NULL) {
-        freeTree(root->left);
-        freeTree(root->right);
-        free(root);
-    }
-}
-
-int main() {
-    // Initialize the root of the binary tree
-    struct TreeNode* root = NULL;
-    int nodeValue;
-    char choice;
-
-    // User input loop to insert nodes into the binary tree
-    do {
-        // Prompt user for a value to insert into the binary tree
-        printf("Input a value to insert into the binary tree: ");
-        scanf("%d", &nodeValue);
-
-        // Insert the value into the binary tree
-        root = insertNode(root, nodeValue);
-
-        // Ask user if they want to insert another node
-        printf("Want to insert another node? (y/n): ");
-        scanf(" %c", &choice);
-
-    } while (choice == 'y' || choice == 'Y');
-
-    // Display the in-order traversal of the binary tree
-    printf("\nIn-order Traversal of the Binary Tree: ");
-    inOrderTraversal(root);
-    printf("\n");
-
-    // Free allocated memory for the binary tree
-    freeTree(root);
-
-    // Return 0 to indicate successful execution
     return 0;
+}
+
+// Function to create a linked list with n nodes
+void createNodeList(int n)
+{
+    struct node *fnNode, *tmp;
+    int num, i;
+    stnode = (struct node *)malloc(sizeof(struct node));
+
+    if(stnode == NULL)  // Check whether stnode is NULL for memory allocation
+    {
+        printf(" Memory can not be allocated.");
+    }
+    else
+    {
+        printf(" Input data for node 1 : ");
+        scanf("%d", &num);
+        stnode-> num = num;      
+        stnode-> nextptr = NULL; // Links the address field to NULL
+        tmp = stnode;
+
+        for(i = 2; i <= n; i++)
+        {
+            fnNode = (struct node *)malloc(sizeof(struct node));
+
+            if(fnNode == NULL)  // Check whether fnNode is NULL for memory allocation
+            {
+                printf(" Memory can not be allocated.");
+                break;
+            }
+            else
+            {
+                printf(" Input data for node %d : ", i);
+                scanf(" %d", &num);
+                fnNode->num = num;      // Links the num field of fnNode with num
+                fnNode->nextptr = NULL; // Links the address field of fnNode with NULL
+                tmp->nextptr = fnNode;  // Links previous node i.e. tmp to the fnNode
+                tmp = tmp->nextptr;
+            }
+        }
+    }
+} 
+
+// Function to delete the first node of the list
+void FirstNodeDeletion()
+{
+    struct node *toDelptr;
+
+    if(stnode == NULL)
+    {
+        printf(" There are no nodes in the list.");
+    }
+    else
+    {
+        toDelptr = stnode;
+        stnode = stnode->nextptr;
+        printf("\n Data of node 1 which is being deleted is :  %d\n", toDelptr->num);
+        free(toDelptr);  // Clears the memory occupied by the first node
+    }
+}
+
+// Function to display the linked list
+void displayList()
+{
+    struct node *tmp;
+
+    if(stnode == NULL)
+    {
+        printf(" No data found in the list.");
+    }
+    else
+    {
+        tmp = stnode;
+        while(tmp != NULL)
+        {
+            printf(" Data = %d\n", tmp->num);   // Prints the data of the current node
+            tmp = tmp->nextptr;                 // Advances the position of the current node
+        }
+    }
 }
