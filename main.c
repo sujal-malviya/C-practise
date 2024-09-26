c8b09ed61fe447cc8246778f40fa98eacfb308f4
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2024-09-27 00:49:13 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,114 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-struct node 
-{
-    int num;                    // Data of the node
-    struct node *nextptr;       // Address of the node
-}*stnode;                       // Pointer to the starting node
+#include <stdio.h> 
 
-// Function prototypes
-void createNodeList(int n);     // Function to create the linked list
-void FirstNodeDeletion();	    // Function to delete the first node
-void displayList();             // Function to display the linked list
+void CountingEvenOdd(int arr[], int arr_size) 
+{ 
+	int even_count = 0; 
+	int odd_count = 0; 
+ 
+	for (int i = 0; i < arr_size; i++) { 
 
-// Main function
-int main()
-{
-    int n, num, pos;
 	
-    printf("\n\n Linked List : Delete first node of Singly Linked List :\n");
-    printf("------------------------------------------------------------\n");
+		if (arr[i] & 1 == 1) 
+			odd_count++; 
+		else
+			even_count++; 
+	} 
 
-    printf(" Input the number of nodes : ");
-    scanf("%d", &n);
-    createNodeList(n);
-
-    printf("\n Data entered in the list are : \n");		
-    displayList();
-
-    FirstNodeDeletion();
-    printf("\n Data, after deletion of first node : \n");		
-    displayList();
-
-    return 0;
-}
-
-// Function to create a linked list with n nodes
-void createNodeList(int n)
-{
-    struct node *fnNode, *tmp;
-    int num, i;
-    stnode = (struct node *)malloc(sizeof(struct node));
-
-    if(stnode == NULL)  // Check whether stnode is NULL for memory allocation
-    {
-        printf(" Memory can not be allocated.");
-    }
-    else
-    {
-        printf(" Input data for node 1 : ");
-        scanf("%d", &num);
-        stnode-> num = num;      
-        stnode-> nextptr = NULL; // Links the address field to NULL
-        tmp = stnode;
-
-        for(i = 2; i <= n; i++)
-        {
-            fnNode = (struct node *)malloc(sizeof(struct node));
-
-            if(fnNode == NULL)  // Check whether fnNode is NULL for memory allocation
-            {
-                printf(" Memory can not be allocated.");
-                break;
-            }
-            else
-            {
-                printf(" Input data for node %d : ", i);
-                scanf(" %d", &num);
-                fnNode->num = num;      // Links the num field of fnNode with num
-                fnNode->nextptr = NULL; // Links the address field of fnNode with NULL
-                tmp->nextptr = fnNode;  // Links previous node i.e. tmp to the fnNode
-                tmp = tmp->nextptr;
-            }
-        }
-    }
+	printf("Number of even elements = %d \nNumber of odd "
+		"elements = %d", 
+		even_count, odd_count); 
 } 
 
-// Function to delete the first node of the list
-void FirstNodeDeletion()
-{
-    struct node *toDelptr;
+int main() 
+{ 
+	int arr[] = { 2, 3, 4, 5, 6 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
 
-    if(stnode == NULL)
-    {
-        printf(" There are no nodes in the list.");
-    }
-    else
-    {
-        toDelptr = stnode;
-        stnode = stnode->nextptr;
-        printf("\n Data of node 1 which is being deleted is :  %d\n", toDelptr->num);
-        free(toDelptr);  // Clears the memory occupied by the first node
-    }
-}
 
-// Function to display the linked list
-void displayList()
-{
-    struct node *tmp;
-
-    if(stnode == NULL)
-    {
-        printf(" No data found in the list.");
-    }
-    else
-    {
-        tmp = stnode;
-        while(tmp != NULL)
-        {
-            printf(" Data = %d\n", tmp->num);   // Prints the data of the current node
-            tmp = tmp->nextptr;                 // Advances the position of the current node
-        }
-    }
+	CountingEvenOdd(arr, n); 
 }
